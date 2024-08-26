920541b08416a1564d28dc5ddafa319be881e973
Christian Carlo M. Galarrita <20210023170@my.xu.edu.ph>
2024-08-26 22:49:30 +0800

    added more details to the ItemCard

--- sample-website/src/App.jsx
+++ sample-website/src/App.jsx
@@ -1,12 +1,16 @@
 import Header from './header.jsx'
 import Aside  from './Aside.jsx'
+import ItemCard from './itemCard.jsx'
 
 function App() {
 
   return (
     <>
     <Header />
+    <div className='flex flex-row'>
     <Aside/>
+    <ItemCard/> 
+    </div>
     </>
   )
 }
--- sample-website/src/itemCard.jsx
+++ sample-website/src/itemCard.jsx
@@ -1,7 +1,25 @@
-function ItemCard () {
+document.getElementById('btn1').onclick = () => {
+    alert("button clicked");
+}
+
+
+function ItemCard() {
     return (
-        <div>
-            
+        <div id="card-container" className="border-2 border-black px-5 py-2 flex flex-row gap-4 flex-wrap">
+
+            <div id="card" className="size-60 border-2 border-black">
+                <div id="card-picture" className="py-2">
+                    <img src="" alt="Item picture" />
+                    <div id="card-information" className="py-7 px-2">
+                        <h2 className="font-mono font-bold">Item 1</h2>
+                        <div id="card-button" className="py-16">
+                            <button className="px-5 py-1 border-2 border-black hover:bg-gray-500 hover:text-white" id="btn1">Like</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+           
         </div>
     )
 }
